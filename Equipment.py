5d1a9dbbf40f2fd0530efd654e5c57f3eb63ff98
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-12-06 15:18:45 -0700

    Update Equipment.py

--- Equipment.py
+++ Equipment.py
@@ -2,20 +2,21 @@ from connection import *
 from Associate import *
 
 class Equipment:
-    def __init__(self, no, amount, condition, name):
+    def __init__(self, no, amount, condition, name, database):
         self.no = no
         self.amount = amount
         self.condition = condition
         self.branch = 1
         self.equipment_name = name
-        createEquip(no, amount, condition, self.branch, name)
+        self.db = database
+        self.db.createEquip(no, amount, condition, self.branch, name)
         
     def updateCondition(self, status):
-        updateEquipCond(self.no, status)
+        self.db.updateEquipCond(self.no, status)
         self.status = status
         
     def addAmount(self):
-        updateEquipAmount(self.no)
+        self.db.updateEquipAmount(self.no)
         self.amount = self.amount +1
     
     def getAmount(self):
